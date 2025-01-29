8aae45de7475cdd127fa024f05cf0208f13e819b
peti <s.peti651155@gmail.com>
2025-01-30 00:02:34 +0100

    basic

---
+++ main/script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggles = document.querySelectorAll('.card-header');
+    const truncateLinks = document.querySelectorAll('.truncate-link');
+
+    toggles.forEach(toggle => {
+        toggle.addEventListener('click', () => {
+            const cardBody = toggle.nextElementSibling;
+
+            if (cardBody.classList.contains('details-hidden')) {
+                cardBody.classList.remove('details-hidden');
+                cardBody.classList.add('details-visible');
+            } else {
+                cardBody.classList.remove('details-visible');
+                cardBody.classList.add('details-hidden');
+            }
+        });
+    });
+
+    truncateLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const fullText = link.dataset.fullText;
+            link.textContent = fullText;
+            link.style.pointerEvents = 'none';
+        });
+    });
+});
