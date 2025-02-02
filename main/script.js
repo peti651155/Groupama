b47f69b53b20cd1381cc219b2f0bb1ed3ac173dd
peti <s.peti651155@gmail.com>
2025-02-02 23:09:27 +0100

    80%

--- main/script.js
+++ main/script.js
@@ -25,3 +25,11 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+document.querySelectorAll('.btn-toggle').forEach(button => {
+    button.addEventListener('click', () => {
+        const entryBody = button.parentElement.nextElementSibling;
+        entryBody.classList.toggle('details-hidden');
+        entryBody.classList.toggle('details-visible');
+    });
+});
